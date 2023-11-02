2c2cec24c083ffce299ceda61e49bad5610b8ca3
Andregit93 <andresevtian501@gmail.com>
2023-11-02 12:44:17 +0700

    Adding Json images

--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -3,17 +3,54 @@ import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:lottie/lottie.dart';
 import 'package:page_transition/page_transition.dart';
 import 'package:coba_flutter/Home.dart';
+import 'dart:async';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  int currentPage = 0;
+  List<String> imageAssets = [
+    'assets/anime1.json',
+    'assets/anime2.json',
+    'assets/anime3.json',
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    Timer.periodic(Duration(seconds: 3), (Timer timer) {
+      if (currentPage < imageAssets.length - 1) {
+        currentPage++;
+      } else {
+        currentPage = 0;
+      }
+      setState(() {});
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double imageWidth = screenWidth;
+
     return AnimatedSplashScreen(
-        splash: Lottie.asset('assets/anime.json'),
-        splashIconSize: 250,
-        backgroundColor: Colors.black,
-        pageTransitionType: PageTransitionType.rightToLeftWithFade,
-        nextScreen: HomePage());
+      splash: Container(
+        width: imageWidth,
+        child: Lottie.asset(
+          imageAssets[currentPage],
+          width: imageWidth,
+        ),
+      ),
+      duration: 25000,
+      splashIconSize: imageWidth,
+      backgroundColor: Colors.black,
+      pageTransitionType: PageTransitionType.rightToLeftWithFade,
+      nextScreen: HomePage(),
+    );
   }
 }
